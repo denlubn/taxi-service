9ebdfc4e1dc8b96cdcd9d3d56b0303bd64c2276e
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-11-12 18:02:35 +0200

    Fix AWS

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -99,13 +99,10 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
 
-# if not DEBUG:
-USE_S3 = os.getenv("USE_S3") == "True"
-
-if USE_S3:
-    AWS_ACCESS_KEY_ID = "AKIAWR7OFLZ2YEQZDI4J"  # os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = "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"  # os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "dennis-pidlubniy-taxi-service"  # os.getenv("AWS_STORAGE_BUCKET_NAME")
+if not DEBUG:
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
